47c2dcb3e69e9b881ed11e9445c6473b5319599e
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2024-05-31 20:50:16 +0530

    add next auth, use google cloud console for auth and modifi sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,14 @@
+"use client"
 import Link from 'next/link';
 import React from 'react'
 import { FaXTwitter } from "react-icons/fa6";
 import { HiHome } from "react-icons/hi";
+import { signIn, signOut } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 
 export default function Sidebar() {
+  const {data: session} = useSession();
+  // this session tell us if the use is authenticated or not.
   return (
     <div className='flex flex-col gap-4'>
         <Link href="/">
@@ -13,7 +18,12 @@ export default function Sidebar() {
         <HiHome className='w-7 h-7'/>
         <span className='font-bold hidden xl:inline'>Home</span>
         </Link>
-        <button className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-300 w-48 h-9 shadow-md hidden xl:inline font-semibold">Sign In</button>
+        {session ? (
+          <button onClick={()=> signOut()} className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-300 w-48 h-9 shadow-md hidden xl:inline font-semibold">Sign Out</button>
+        ) : (
+          <button onClick={()=> signIn()} className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-300 w-48 h-9 shadow-md hidden xl:inline font-semibold">Sign In</button>
+        )}
+        
     </div>
   )
 }
